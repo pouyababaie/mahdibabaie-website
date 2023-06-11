bb58356565722beb96565fadfaa59adac9d2c2c0
pouyababaie <pouyababie123@gmail.com>
2023-06-11 16:34:02 +0330

    init

---
+++ apps/ui/src/app/app.tsx
@@ -0,0 +1,13 @@
+
+
+import {Header} from '@mahdibabaie-website/ui-shared'
+
+export default function App() {
+
+  return (
+    <>
+      <Header/>
+    </>
+)
+  ;
+}
---
+++ libs/ui-shared/src/lib/header/header.tsx
@@ -0,0 +1,18 @@
+import styles from './header.module.scss';
+import * as React from 'react';
+
+/* eslint-disable-next-line */
+export interface HeaderProps {
+}
+
+export function Header(props: HeaderProps) {
+
+
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default Header;
